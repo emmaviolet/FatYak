24506ed59e300e6cee3b0c3a7fee36cb3c9c5a20
emmaviolet <emmavioletmakinson@gmail.com>
2017-09-11 17:17:43 +0100

    Initial Commit

---
+++ FatYak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FatYak
+//
+//  Created by Emma Makinson on 11/09/2017.
+//  Copyright (c) 2017 Emma Makinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
